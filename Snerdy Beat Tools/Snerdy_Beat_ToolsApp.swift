62b8d47b337fbfccd6198f0d9d8b88ebb769470b
JackPadalino <pada0867@gmail.com>
2023-12-08 08:24:15 -0500

    init kermit

---
+++ Snerdy Beat Tools/Snerdy_Beat_ToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Snerdy_Beat_ToolsApp.swift
+//  Snerdy Beat Tools
+//
+//  Created by Jack Padalino on 12/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Snerdy_Beat_ToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
